732498eb6f8db300cf2c08fc25dad8e29b67f347
chigiriyap <slava.goroh.99@gmail.com>
2025-10-07 11:10:58 +0300

    Fix the brain_calc game error

--- brain_games/scripts/brain_calc_cli.py
+++ brain_games/scripts/brain_calc_cli.py
@@ -2,6 +2,7 @@ from brain_games.engine import play
 from brain_games.games import brain_calc
 
 def main():
+    print("What is the result of the expression?")
     play(brain_calc.generate_question)
 
 if __name__ == '__main__':
